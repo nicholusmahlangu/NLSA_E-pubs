0de51ebbdcb98263d8142ce91e1932c237e4b711
nicholusmahlangu <nicholus.mahlangu@nlsa.ac.za>
2024-12-04 14:42:00 +0200

    Initial commit 4/12/2024

---
+++ application files/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>NLSA - E-pubs</title>
+</head>
+<body>
+    <nav class="nav">
+        <div class="nav-logo">
+            <p>logo</p>
+        </div>
+
+        <div class="nav-menu">
+            <ul>
+                <li><a href="#" class="link active">Home</a></li>
+                <li><a href="#" class="link">Overview</a></li>
+                <li><a href="#" class="link">Form</a></li>
+            </ul>
+        </div>
+        
+        <div class="nav-button">
+            <button class="btn" id="loginBtn">Sign In</button>
+            <button class="btn" id="signupBtn">Sign Up</button>
+        </div>
+
+        <div class="nav-menu-btn">
+            <i class='bx bx-menu'></i>
+        </div>
+    </nav>
+    
+</body>
+</html>
