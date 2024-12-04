bbf619e33dc3bdc0616ab4c03d49864caf8e830f
nicholusmahlangu <nicholus.mahlangu@nlsa.ac.za>
2024-12-04 15:31:12 +0200

    Adding of pictures

--- application files/index.php
+++ application files/index.php
@@ -3,32 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Tab icon -->
+    <link href="../assets/img/favicon.webp" rel="icon">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="../assets/css/style.css">
     <title>NLSA - E-pubs</title>
 </head>
 <body>
-    <nav class="nav">
-        <div class="nav-logo">
-            <p>logo</p>
-        </div>
+    <div class="wrapper">
+        <nav class="nav">
+            <div class="nav-logo">
+                <p>logo</p>
+            </div>
 
-        <div class="nav-menu">
-            <ul>
-                <li><a href="#" class="link active">Home</a></li>
-                <li><a href="#" class="link">Overview</a></li>
-                <li><a href="#" class="link">Form</a></li>
-            </ul>
-        </div>
-        
-        <div class="nav-button">
-            <button class="btn" id="loginBtn">Sign In</button>
-            <button class="btn" id="signupBtn">Sign Up</button>
-        </div>
+            <div class="nav-menu">
+                <ul>
+                    <li><a href="#" class="link active">Home</a></li>
+                    <li><a href="#" class="link">Overview</a></li>
+                    <li><a href="#" class="link">Form</a></li>
+                </ul>
+            </div>
+            
+            <div class="nav-button">
+                <button class="btn" id="loginBtn">Sign In</button>
+                <button class="btn" id="signupBtn">Sign Up</button>
+            </div>
 
-        <div class="nav-menu-btn">
-            <i class='bx bx-menu'></i>
-        </div>
-    </nav>
-    
+            <div class="nav-menu-btn">
+                <i class='bx bx-menu'></i>
+            </div>
+        </nav>
+    </div>
 </body>
 </html>
